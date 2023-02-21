5a0adb9dfcefe2395de55259410f931b247b637c
itsRafey <askrafey@gmail.com>
2023-02-21 22:08:07 +0500

    while loop

--- day-1.py
+++ day-1.py
@@ -2,11 +2,20 @@
 arr = [1,2,3,4,5]
 
 arr.append(6)
+print(arr)
 # specified index
 arr.insert(3,7)
-
+print(arr)
 # to delete at the end of index
 arr.pop(2)
 
 #delete end of array
 arr.pop()
+print(arr)
+
+arr = [6,7,8,9,0]
+i = 1
+while i < len(arr): # to print length of array
+ 
+    print(arr[i])
+    i += 1
