6d8ba7a092127e3d9c8a1a6b298ac5be5950a8a5
itsRafey <askrafey@gmail.com>
2023-02-21 21:54:16 +0500

    writing methods

--- day-1.py
+++ day-1.py
@@ -1,1 +1,12 @@
-print("hello world")
+# insert
+arr = [1,2,3,4,5]
+
+arr.append(6)
+# specified index
+arr.insert(3,7)
+
+# to delete at the end of index
+arr.pop(2)
+
+#delete end of array
+arr.pop()
